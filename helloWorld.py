39587e972e224c906527ff8707e16df522d76742
Anirudh Reddy <ranirudh702@gmail.com>
2024-01-06 07:32:43 -0800

    Did a practice program to calculate percentage of total profit

---
+++ helloWorld.py
@@ -0,0 +1,6 @@
+#program to calculate profit as a percentage of total sales
+percentageOfAnnualProfit = .23
+totalSales = float(input("Please enter the total sales as a number with no commas or spaces. Decimal points are allowed.\n"))
+print(totalSales)
+profit = totalSales * .23
+print(profit)
